1dbbde5e377328f0e9a8cf8d1991a99d297a2501
Aaron Neal <neal.aaron.m@gmail.com>
2022-04-28 11:14:45 -0400

    completed exercise.  no loops used.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    let reverseArr = arr.reverse();
+    return reverseArr.join('');
 };
+
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+/*  for (let i = 0; i <= content.length; i++)
+        var contentArray = [content.slice(i)];
+        contentArray = contentArray.reverse();
+        return contentArray.toString(); */;
+
